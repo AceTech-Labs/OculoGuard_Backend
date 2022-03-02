d270150998dcf92d0752fe7ca66378f73c7aa394
Thidas Jayawardane <thidas.2019717@iit.ac.lk>
2022-03-02 21:46:20 +0530

    Update welcome_page.dart

--- oculoguard_mobile_app_flutter/lib/screens/welcome_page.dart
+++ oculoguard_mobile_app_flutter/lib/screens/welcome_page.dart
@@ -7,7 +7,8 @@ import '../widgets/widget.dart';
 class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var height = MediaQuery.of(context).size.height;
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
 
     return Scaffold(
       body: SafeArea(
