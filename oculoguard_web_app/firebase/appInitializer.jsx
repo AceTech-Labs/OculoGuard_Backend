b86041e4ae6b255f374723ad32678375be737dab
Thidas Jayawardane <thidas.2019717@iit.ac.lk>
2022-03-06 18:22:01 +0530

    added firebase to nextJS

---
+++ oculoguard_web_app/firebase/appInitializer.jsx
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const credentials = {
+  apiKey: "AIzaSyBc9oCslwTk7pYqBROBh2-EXsmhSqgCoyk",
+  authDomain: "oculoguard.firebaseapp.com",
+  projectId: "oculoguard",
+  storageBucket: "oculoguard.appspot.com",
+  messagingSenderId: "554127335861",
+  appId: "1:554127335861:web:12503480cfd189145ff9fc",
+  measurementId: "G-LESJ69QM8L",
+};
+
+const app = initializeApp(credentials);
