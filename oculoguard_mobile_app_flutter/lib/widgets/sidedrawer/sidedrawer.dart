aa755f59ff0850bc6c9629dd5e7cc782a2278cea
Thidas Jayawardane <thidas.2019717@iit.ac.lk>
2022-03-27 21:18:07 +0530

    added amplify configuration for flutter

--- oculoguard_mobile_app_flutter/lib/widgets/sidedrawer/sidedrawer.dart
+++ oculoguard_mobile_app_flutter/lib/widgets/sidedrawer/sidedrawer.dart
@@ -1,11 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:oculoguard_mobile_app_flutter/constants.dart';
 
 class SideDrawer extends StatelessWidget {
   SideDrawer({Key? key}) : super(key: key);
-
-  User? user = FirebaseAuth.instance.currentUser;
 
   @override
   Widget build(BuildContext context) {
@@ -15,16 +12,16 @@ class SideDrawer extends StatelessWidget {
         Size size = MediaQuery.of(context).size;
         return ListView(
           children: [
-            CircleAvatar(
-              backgroundImage: NetworkImage(
-                user!.photoURL!,
-                scale: 1,
-              ),
-              radius: size.width / 4,
-            ),
-            ListTile(
-              title: Text(user!.displayName!),
-            )
+            // CircleAvatar(
+            //   backgroundImage: NetworkImage(
+            //     user!.photoURL!,
+            //     scale: 1,
+            //   ),
+            //   radius: size.width / 4,
+            // ),
+            // ListTile(
+            //   title: Text(user!.displayName!),
+            // )
           ],
         );
       }),
