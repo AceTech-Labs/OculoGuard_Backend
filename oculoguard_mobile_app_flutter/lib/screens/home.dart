f41e553fbc3a73410c34a65da7f7b33d8f051175
Thidas Jayawardane <thidas.2019717@iit.ac.lk>
2022-05-02 03:31:52 +0530

    home page of the app

--- oculoguard_mobile_app_flutter/lib/screens/home.dart
+++ oculoguard_mobile_app_flutter/lib/screens/home.dart
@@ -1,4 +1,7 @@
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:oculoguard_mobile_app_flutter/widgets/record.dart';
+import '../widgets/my_text_button.dart';
 import 'custom_appbar.dart';
 import 'package:oculoguard_mobile_app_flutter/widgets/sidedrawer/sidedrawer.dart';
 
@@ -15,19 +18,40 @@ class _HomeState extends State<Home> {
     super.initState();
   }
 
+// void getData() async {
+//   try{
+//     final request = Model
+//   }
+//   on Exception catch(e){
+
+//   }
+// }
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-        onWillPop: () async => false,
-        child: Scaffold(
-          // appBar: AppBar(
-          //   // automaticallyImplyLeading: false,
-          //   elevation: 0,
-          //   backgroundColor: Colors.transparent,
-          //   title: Text("OculoGuard"),
-          // ),
-          drawer: SideDrawer(),
-          body: customScrollAppBar(context),
-        ));
+      onWillPop: () async => false,
+      child: Scaffold(
+        // appBar: AppBar(
+        //   elevation: 0,
+        //   backgroundColor: Colors.transparent,
+        // ),
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              title: Text("OculoGuard"),
+              automaticallyImplyLeading: false,
+              leading: IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () => {_scaffoldKey.currentState!.openDrawer()},
+              ),
+            )
+          ],
+        ),
+        drawer: SideDrawer(),
+      ),
+    );
   }
 }
