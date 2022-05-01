7669480da10d80da7e1d0e1ff1db7ea797630bd3
Thidas Jayawardane <thidas.2019717@iit.ac.lk>
2022-05-02 03:33:15 +0530

    side menu updated

--- oculoguard_mobile_app_flutter/lib/widgets/sidedrawer/sidedrawer.dart
+++ oculoguard_mobile_app_flutter/lib/widgets/sidedrawer/sidedrawer.dart
@@ -1,3 +1,4 @@
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:oculoguard_mobile_app_flutter/constants.dart';
 
@@ -12,16 +13,24 @@ class SideDrawer extends StatelessWidget {
         Size size = MediaQuery.of(context).size;
         return ListView(
           children: [
-            // CircleAvatar(
-            //   backgroundImage: NetworkImage(
-            //     user!.photoURL!,
-            //     scale: 1,
-            //   ),
-            //   radius: size.width / 4,
-            // ),
-            // ListTile(
-            //   title: Text(user!.displayName!),
-            // )
+            ListTile(
+              title: Text("name"),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Settings"),
+              onTap: () => {},
+            ),
+            ListTile(
+              title: const Text("SignOut"),
+              leading: const Icon(Icons.logout),
+              onLongPress: () => {
+                Amplify.Auth.signOut().then((value) => {
+                      Navigator.pushNamedAndRemoveUntil(
+                          context, "/", (route) => false)
+                    })
+              },
+            )
           ],
         );
       }),
