9e901693526b393811d34cd606ebbe8266307a4a
Thidas Jayawardane <thidas.2019717@iit.ac.lk>
2022-05-02 03:29:08 +0530

    backend configured with amplify

--- oculoguard_mobile_app_flutter/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ oculoguard_mobile_app_flutter/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,11 +3,20 @@ export type AmplifyDependentResourcesAttributes = {
         "oculoguard2251e9ec": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
             "AppClientIDWeb": "string",
             "AppClientID": "string"
+        }
+    },
+    "api": {
+        "oculoguard": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
         }
     }
 }
