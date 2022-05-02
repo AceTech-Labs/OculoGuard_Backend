54e685f755f519def7eb4a33ceb529db41f886cc
ChamodhS <chamodhsamaranayake928@gmail.com>
2022-05-02 12:13:25 +0530

    Evaluation model imported

--- Backend/Servers/Diagnosis/dme-api/app.py
+++ Backend/Servers/Diagnosis/dme-api/app.py
@@ -1,14 +1,33 @@
-from flask import Flask 
-
+from flask import Flask, jsonify, request
+import torch
+import torch.nn as nn
+from torchvision import transforms
+from PIL import Image
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World"
+@app.route('/diagnosis/bone', methods = ['GET', 'POST'])
+def bone_diagnosis():
+    if request.method == 'POST':     #stuff inside this should be changed 
+        model = torch.load('./bone-diagnosis.pt')           #
+        image = Image.open(request.files['file'].stream)
+        test_transforms = transforms.Compose(
+          [
+            transforms.CenterCrop(224),
+            transforms.ToTensor()
+          ]
+        )
 
+        input = test_transforms(image)
+        input = input.view(1, 3, 224, 224)
+        output = model(input)
+        prediction = int(torch.max(output.data, 1)[1].numpy())
 
+        return jsonify(
+          result = 'No cancer tumor present' if prediction == 0 else 'A cancer tumor has been detected',
+        )
 
+    return None 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+  app.run(debug=False)
---
+++ Backend/Servers/Diagnosis/dme-api/dme-glau-model.py
@@ -0,0 +1,64 @@
+import numpy as np
+np.random.seed(2017)
+from keras.preprocessing import image
+from keras.applications.imagenet_utils import preprocess_input
+from keras.models import Model
+import tkinter as tk
+from tkinter import filedialog
+import tensorflow as tf
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Model
+
+
+
+
+
+
+dict={0:'Glaucoma_Positive',1:'Macular_Edema',2:'Normal',3:'dr_Mild',4:'dr_Moderate',5:'dr_Proliferate',6:'dr_Severe'}
+model_1=('C:/Users/Harrisagar/Desktop/FinalClassification/Final_Prediction/Model 01 New/models/model.h5')
+mobile = tf.keras.applications.mobilenet.MobileNet()
+
+
+x = mobile.layers[-6].output
+
+GVP =  tf.keras.layers.GlobalAveragePooling2D()(x)
+output = Dense(units=7, activation='softmax')(GVP)
+
+model_1 = Model(inputs=mobile.input, outputs=output)
+
+model_2 = Model(inputs=mobile.input, outputs=output)
+
+for layer in model_1.layers[:-23]:
+    layer.trainable = False
+
+for layer in model_2.layers[:-23]:
+    layer.trainable = False
+    
+
+
+model_1.compile(optimizer=Adam(learning_rate=0.0003), loss='categorical_crossentropy', metrics=['accuracy'])
+model_1.load_weights('C:/Users/Harrisagar/Desktop/FinalClassification/Final_Prediction/Model 01 New/models/model.h5')
+
+
+
+root = tk.Tk()
+root.withdraw()
+img_path = filedialog.askopenfilename()
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+x = tf.keras.applications.mobilenet.preprocess_input(x)
+pr=model_1.predict(x)
+pr1=np.argmax(pr)
+
+
+if dict[pr1] == ("Glaucoma_Positive"):
+    print("Glaucoma_Positive")
+elif dict[pr1] ==("Macular_Edema"):
+    print("Macular_Edema")
+else:
+    print("No Disease")
